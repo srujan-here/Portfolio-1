969e4028dcad34d271cad1a058ac1f70e864e7c7
Srujan <puttasrujan20bcs74@iiitkottayam.ac.in>
2023-02-02 08:52:23 +0530

    added schemas in sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+import PageInfo from './PageInfo'
+import Experience from './Experience'
+import Project from './Project'
+import Skill from './Skill'
+import Social from './Social'
+export const schemaTypes = [PageInfo,Experience,Project,Skill,Social]
