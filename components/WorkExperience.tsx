0bf4ab6f14e1fe544c5f14f5896e835072c0cd4c
Srujan <puttasrujan20bcs74@iiitkottayam.ac.in>
2023-02-03 11:09:42 +0530

    saved

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -11,7 +11,7 @@ export const WorkExperience = (props: Props) => {
            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl mb-10">
            Experience
            </h3>
-           <div className='w-full  flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory mt-20 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+           <div className='w-full  flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory mt-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
             <ExperienceCard  />
             <ExperienceCard />
            
